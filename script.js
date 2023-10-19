f70098ef063e7193b123e17e3af41b9346846c8e
michaelwahyudin <147363372+michaelwahyudin@users.noreply.github.com>
2023-10-19 19:40:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function onButtonClick() {
-    alert('Silver Chariot!!');
-  }
+
   
 const button = document.querySelector('button');
 button.addEventListener('click', onButtonClick);
+
+
+
+const name = prompt("Who... are you?");
+console.log(name);
+
+function onButtonClick() {
+  alert(`hello ${name}`);
+}
