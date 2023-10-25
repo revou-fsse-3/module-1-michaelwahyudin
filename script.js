bbedd6bd87b168eb258e5c5da1996fa5340b01ea
michael wahyudin <michaelwahyudin90@gmail.com>
2023-10-25 12:32:27 +0700

    Change for new assignment

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+let slideIndex = 0;
+showSlides();
 
-  
-const button = document.querySelector('button');
-button.addEventListener('click', onButtonClick);
-
-
-
-const name = prompt("Who... are you?");
-console.log(name);
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every x s
+}
+const button = document.querySelector("button");
+button.addEventListener("click", onButtonClick);
 
 function onButtonClick() {
-  alert(`hello ${name}`);
+  alert(`Error 404`);
 }
